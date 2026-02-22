4cf027ca14ae1c9402cb6ba24bc9d85cd01a21e1
Ahmad Arceo <3794564+ahmadarceo@users.noreply.github.com>
2026-02-22 20:38:16 +0800

    Update

--- opentofu/main.tf
+++ opentofu/main.tf
@@ -6,3 +6,36 @@ terraform {
     }
   }
 }
+
+variable "zone_id" {
+  type      = string
+  sensitive = true
+}
+
+variable "server_ip" {
+  type = string
+}
+
+resource "cloudflare_dns_record" "apex" {
+  zone_id = var.zone_id
+  type    = "A"
+  name    = "@"
+  content = var.server_ip
+  ttl     = 1
+  proxied = true
+}
+
+resource "cloudflare_dns_record" "www" {
+  zone_id = var.zone_id
+  type    = "A"
+  name    = "www"
+  content = var.server_ip
+  ttl     = 1
+  proxied = true
+}
+
+resource "cloudflare_zone_setting" "always_online" {
+  zone_id    = var.zone_id
+  setting_id = "always_online"
+  value      = "on"
+}
